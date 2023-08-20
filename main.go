a15c18c2d07960aa1e212f7813e9551d1a31623f
marioteik <mario.teik@gmail.com>
2023-08-20 13:22:15 -0300

    add nested components

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package indigoo
 
 import (
 	"errors"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"html/template"
 	"log"
@@ -9,7 +10,14 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
+
+type Page struct {
+	Template string
+	// Components: [[componentPath, []componentNames],[componentPath, []componentNames],[componentPath, []componentNames]]
+	Components [][]interface{}
+}
 
 var appFolder string
 var baseTemplate string
@@ -63,15 +71,15 @@ func renderRoutesFromFolderStructure(mux *chi.Mux) *chi.Mux {
 }
 
 func renderPage(w http.ResponseWriter, pagePath string) error {
-	page, components, err := createPageTemplate(pagePath)
+	page, err := createPageTemplate(pagePath)
 	if err != nil {
 		return err
 	}
 
-	log.Println(components)
+	log.Println(page.Template)
 
 	name := filepath.Base(pagePath)
-	ts, err := template.New(name).Parse(*page)
+	ts, err := template.New(name).Parse(page.Template)
 	if err != nil {
 		return err
 	}
@@ -86,21 +94,85 @@ func renderPage(w http.ResponseWriter, pagePath string) error {
 	return nil
 }
 
-func createPageTemplate(pagePath string) (*string, *[]string, error) {
-	file, err := os.ReadFile(pagePath)
-	if err != nil {
-		return nil, nil, err
+func createPageTemplate(pagePath string) (*Page, error) {
+	page := Page{
+		Template:   "{{template \"base\" .}}\n",
+		Components: [][]interface{}{},
 	}
 
-	componentPaths := []string{}
-	fileString := string(file)
-	fileString = regexp.MustCompile("import\\s+\\(([^)]+)\\)").ReplaceAllStringFunc(fileString, func(s string) string {
-		componentPaths = append(componentPaths, regexp.MustCompile("\"(.+)\"").FindStringSubmatch(s)[1])
-		return ""
-	})
+	parsedPage, err := parsePageTemplate(pagePath)
+	if err != nil {
+		return nil, err
+	}
 
-	tmpl := "{{template \"base\" .}}\n"
-	tmpl += fileString
+	page.Template += *parsedPage
+
+	componentPathPattern := regexp.MustCompile(`^\s*import\s+.*from\s+'([^']+)';\s*$`)
+
+	fileLines := strings.Split(page.Template, "\n")
+	var newTemplateLines []string
+	for _, line := range fileLines {
+		match := componentPathPattern.FindStringSubmatch(line)
+
+		if len(match) > 0 {
+			componentNamePattern := regexp.MustCompile(`(\s|{|,)[A-Z]+[^.,\s,}]+`)
+
+			componentMatches := componentNamePattern.FindAllString(line, -1)
+
+			if len(componentMatches) > 0 {
+				var componentNames []string
+				for _, componentMatch := range componentMatches {
+					componentNames = append(componentNames, componentMatch[1:])
+				}
+
+				componentPath := filepath.Join(filepath.Dir(pagePath), match[1])
+
+				page.Components = append(page.Components, []interface{}{componentPath, componentNames})
+			}
+		} else {
+			newTemplateLines = append(newTemplateLines, line)
+		}
+	}
+
+	page.Template = strings.Join(newTemplateLines, "\n")
+
+	for _, component := range page.Components {
+		componentPath := component[0].(string)
+		componentNames := component[1].([]string)
+
+		for _, componentName := range componentNames {
+			componentTemplate, err := parseComponentTemplate(componentPath, componentName)
+			if err != nil {
+				return nil, err
+			}
+
+			componentTagPattern := regexp.MustCompile(`<` + componentName + `\s*(.*?)<\/` + componentName + `>|<` + componentName + `\s*(.*?)\/>`)
+
+			page.Template = componentTagPattern.ReplaceAllStringFunc(page.Template, func(match string) string {
+				matches := componentTagPattern.FindStringSubmatch(match)
+
+				for _, match := range matches {
+					templateRef := "{{template \"components/" + componentName + "\" .}}"
+					return strings.Replace(match, match, templateRef, 1)
+				}
+
+				return match
+			})
+
+			page.Template += *componentTemplate
+		}
+	}
+
+	return &page, nil
+}
+
+func parsePageTemplate(pagePath string) (*string, error) {
+	file, err := os.ReadFile(pagePath)
+	if err != nil {
+		return nil, err
+	}
+
+	tmpl := string(file)
 
 	if regexp.MustCompile("<script>").MatchString(tmpl) {
 		tmpl = regexp.MustCompile("<script>").ReplaceAllString(tmpl, "{{define \"js\"}}\n<script>")
@@ -118,7 +190,36 @@ func createPageTemplate(pagePath string) (*string, *[]string, error) {
 		tmpl += "{{end}}"
 	}
 
-	return &tmpl, &componentPaths, nil
+	return &tmpl, nil
+}
+
+func parseComponentTemplate(componentPath string, componentName string) (*string, error) {
+	file, err := os.ReadFile(componentPath)
+	if err != nil {
+		return nil, err
+	}
+
+	tmpl := string(file)
+
+	if regexp.MustCompile("<script>").MatchString(tmpl) {
+		tmpl = regexp.MustCompile("<script>").ReplaceAllString(tmpl, "{{define \"js\"}}\n<script>")
+		tmpl = regexp.MustCompile("</script>").ReplaceAllString(tmpl, fmt.Sprintf("</script>\n{{define \"components/%s\"}}\n", componentName))
+		tmpl = regexp.MustCompile("</script>").ReplaceAllString(tmpl, "</script>\n{{end}}\n")
+	} else {
+		tmpl = fmt.Sprintf("{{define \"components/%s\"}}\n", componentName) + tmpl
+	}
+
+	if regexp.MustCompile("<style>").MatchString(tmpl) {
+		tmpl = regexp.MustCompile("<style>").ReplaceAllString(tmpl, "{{end}}\n<style>")
+		tmpl = regexp.MustCompile("<style>").ReplaceAllString(tmpl, "{{define \"css\"}}\n<style>")
+		tmpl = regexp.MustCompile("</style>").ReplaceAllString(tmpl, "</style>\n{{end}}\n")
+	} else {
+		tmpl += "{{end}}"
+	}
+
+	log.Println(tmpl)
+
+	return &tmpl, nil
 }
 
 func generateBaseTemplate(path string) string {
